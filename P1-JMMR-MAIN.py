a413b17ce055acb2eb244cac8cf20db71cc47267
JM1897962 <1897962.jmmr@gmail.com>
2023-09-28 18:35:42 -0600

    Realizacion de la practica 1 y 2

---
+++ P1-JMMR-MAIN.py
@@ -0,0 +1,15 @@
+import requests  # Realiza las solicitudes en HTTP
+from bs4 import BeautifulSoup  # Analiza el contenido del HTML
+import re  # Trabaja con expresiones regulares
+import urllib.request  # Descargar los datos
+url = "https://datos.cdmx.gob.mx/tl/dataset/certificados-de-defuncion-sedesa"
+
+
+#        |Analiza la cosa|PeticionGet|                                  |Busca que termine en .csv| |Obtiene el link wiiii
+csv_link = BeautifulSoup(requests.get(url).text, 'html.parser').find('a', href=re.compile(r'\.csv$')).get('href')
+
+# La descarga
+urllib.request.urlretrieve(csv_link, "Defunciones-Covid.csv")
+
+#Juan Manuel Martínez Ramírez
+#1897962
