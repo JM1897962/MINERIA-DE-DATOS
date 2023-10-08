e745bd17dfeeec0dd30c8795cbc5e4f31a55e2f4
JM1897962 <1897962.jmmr@gmail.com>
2023-10-08 14:05:55 -0600

    Realizacion de la practica 5 y 6

---
+++ P5-JMMR-MAIN.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+
+df = pd.read_csv('Defunciones-Covid-A2.csv', nrows=22000)
+
+modelo = ols('edad ~ fecha_defuncion', data=df).fit()
+tabla_de_anova = sm.stats.anova_lm(modelo, typ=2)
+
+print("Tabla de ANOVA:")
+print(tabla_de_anova)
---
+++ P6-JMMR-MAIN.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Defunciones-Covid-A2.csv', nrows=185000)
+df['fecha_defuncion'] = pd.to_datetime(df['fecha_defuncion'])
+fecha_final =  df['fecha_defuncion'].iloc[0]
+df['dias_tras_la_muerte'] = (df['fecha_defuncion'] - fecha_final).dt.days
+df_promedio = df.groupby('dias_tras_la_muerte')['edad'].mean().reset_index()
+
+X = sm.add_constant(df_promedio['dias_tras_la_muerte'])
+Y = df_promedio['edad']
+
+modelo = sm.OLS(Y, X).fit()
+
+print(modelo.summary())
+plt.figure(figsize=(10, 6))
+plt.scatter(df_promedio['dias_tras_la_muerte'], df_promedio['edad'], label='Promedios')
+plt.plot(df_promedio['dias_tras_la_muerte'], modelo.predict(X), color='red', label='Recta')
+plt.xlabel('Días Transcurridos desde {}'.format(fecha_final.date()))
+plt.ylabel('Promedio de la Edad de los Fallecidos')
+plt.title('Regresion lineal: DTLM vs. EDLF')
+plt.legend()
+
+plt.yticks(range(int(min(Y)), int(max(Y))+1))
+plt.savefig('Regresion lineal de los Fallecidos.png')
+plt.tight_layout()
+plt.show()
